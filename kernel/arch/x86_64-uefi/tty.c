ff66aa631fe6e9340a1b90a174d4e53ae1962f30
Cristian <assemblyislaw@gmail.com>
2018-01-09 18:03:12 +0100

    Add files via upload

--- kernel/arch/x86_64-uefi/tty.c
+++ kernel/arch/x86_64-uefi/tty.c
@@ -23,42 +23,46 @@ KABI void set_pixel(int w, int h, uint32_t rgb)
 
 KABI void draw_line(int x1, int y1, int x2, int y2, uint32_t rgb)  
 {
-    int delta_y = y2 - y1;
-    int delta_x = x2 - x1;
-   	if (x1 != x2) /* The line it's not vertical */
-    	if (x1 < x2) /* A line that goes from x1 to x2 */
-    	{
-    		/* Apply the general law for lines */ 
-    		for (int x = x1; x < x2; x++){
-    			int y = y1 + delta_y * (x - x1) / delta_x;
-    			set_pixel(x, y, rgb);
-    		}
-    	}
-    	if (x1 > x2) /* A line that goes from x2 to x1 */
-    	{
-    		/* Apply the general law for lines */ 
-    		for (int x = x2; x < x1; x++){
-    			int y = y2 + delta_y * (x - x2) / delta_x;
-    			set_pixel(x, y, rgb);
-    		}
-    	}
-    if (x1 == x2) /* Vertical line */
-    	{
-    		if (y1 < y2)
-    		{
-    			while (y1 <= y2){
-    				set_pixel(x1, y1, rgb);
-    				y1 ++;
-    			}
-    		}
-    		if (y1 > y2)
-    		{
-    			while (y1 >= y2){
-        			set_pixel(x2, y2, rgb);
-                	y2 ++;
-        		}
-    		}
-    }
+	int delta_y = y2 - y1;
+	int delta_x = x2 - x1;
+	if (x1 != x2)  /* The line it's not vertical */
+	{
+		if (x1 < x2) /* A line that goes from x1 to x2 */
+		{
+			/* Apply the general law for lines */ 
+			for (int x = x1; x < x2; x++){
+				int y = y1 + delta_y * (x - x1) / delta_x;
+				set_pixel(x, y, rgb);
+			}
+		}
+		if (x1 > x2) /* A line that goes from x2 to x1 */
+		{
+			/* Apply the general law for lines */ 
+			for (int x = x2; x < x1; x++){
+				int y = y2 + delta_y * (x - x2) / delta_x;
+				set_pixel(x, y, rgb);
+			}
+		}
+	}
+	
+	if (x1 == x2) /* Vertical line */
+	{
+		if (y1 < y2)
+		{
+			while (y1 <= y2){
+				set_pixel(x1, y1, rgb);
+				y1 ++;
+			}
+		}
+		
+		if (y1 > y2)
+		{
+			while (y1 >= y2){
+				set_pixel(x2, y2, rgb);
+				y2 ++;
+			}
+		}
+	}
 }
 
 KABI void draw_rect(int x1, int y1, int x2, int y2, uint32_t rgb)
@@ -74,9 +78,9 @@ KABI void tty_init(void)
 {
 	// Colors
 	uint32_t r = 64 << 16;
-    uint32_t g = 64 << 8;
-    uint32_t b = 64;
+	uint32_t g = 64 << 8;
+	uint32_t b = 64;
 
-    // Fill the screen
-    fill_screen_GOP(&archmain.graphics, r | g | b);
+	// Fill the screen
+	fill_screen_GOP(&archmain.graphics, r | g | b);
 }
