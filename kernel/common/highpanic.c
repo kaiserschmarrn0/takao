367687a88ccd874d37d7a270b77c5cb066ef3106
TheStr3ak5 <assemblyislaw@gmail.com>
2018-01-12 20:49:50 +0100

    error reporting

--- kernel/common/highpanic.c
+++ kernel/common/highpanic.c
@@ -5,9 +5,13 @@
 // License: GNU GPL v2, check LICENSE file under the distributed package for details.
 
 #include "../includes/utils/highpanic.h"
+#include "../includes/output/serial.h"
 
 KABI void panic(int error_code)
 {
+	serial_print("[panic] Kernel halted due to an error!\n");
+	serial_print("\tError code :");
+	serial_print("\n");
 	// Stuff here
 	for (;;);
 }
