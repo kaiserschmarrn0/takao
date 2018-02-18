fd7dbf05351ada48dd0b7418bdc8fa431f057467
Cristian <assemblyislaw@gmail.com>
2018-02-18 17:39:59 +0100

    Update cxa_pure_virtual.cpp

--- kernel/glue/cxa_pure_virtual.cpp
+++ kernel/glue/cxa_pure_virtual.cpp
@@ -4,6 +4,8 @@
 //              if we want to use pure virtual functions, despite it is empty
 //              almost always, and does not need headers, just needs to be
 //              linked.
+//              The function is only called in case a pure virtual function call 
+//              cannot be made
 
 // Copyright 2018 The Takao Authors (AUTHORS.md). All rights reserved.
 // Use of this source code is governed by a license that can be
