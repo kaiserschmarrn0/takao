66d6d3cdfcb0eaa7e3223c59622c4019f22830d3
mintsuki <mintsuki@protonmail.com>
2019-09-14 18:20:57 +0200

    Make spinlock functions inlined

--- source/kernel/lib/spinlock.d
+++ source/kernel/lib/spinlock.d
@@ -12,14 +12,14 @@ immutable SpinLock locked   = 1; /// Locked spinlock value
 /// Maximum spins that a spinlock will allow before being considered a deadlock
 immutable auto deadlockCounter = 0x4000000;
 
-void acquireSpinlock(shared(SpinLock)* lock) {
+pragma(inline, true) void acquireSpinlock(shared(SpinLock)* lock) {
     foreach (i; 0..deadlockCounter) {
         if (cas(lock, unlocked, locked)) {
             return;
         }
     }
 
-    panic("Deadlock after %u itinerations", deadlockCounter);
+    panic("Deadlock after %u iterations", deadlockCounter);
 }
 
 /**
@@ -28,6 +28,6 @@ void acquireSpinlock(shared(SpinLock)* lock) {
  * Params:
  *     lock  = Lock to release
  */
-void releaseSpinlock(shared(SpinLock)* lock) {
+pragma(inline, true) void releaseSpinlock(shared(SpinLock)* lock) {
     atomicStore(*lock, unlocked);
 }
